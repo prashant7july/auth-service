e73b6c838332ed23fb457c5aa106de5d930dcc47
prashant7july <prashant7july@gmail.com>
2022-09-11 17:45:35 +0530

    Modify OTP Routes

--- modules/otp/otp.route.js
+++ modules/otp/otp.route.js
@@ -1,15 +1,10 @@
 const { Router } = require('express');
-//var { expressjwt: jwt } = require("express-jwt")
-
 const otpCtrl = require('./otp.controller');
-const config = require('./../../config/config');
-
+const requireAuth = require('./../../helpers/auth.middleware');
 const router = Router()
-//const jwtConfig = jwt({ secret: config.jwtSecret, algorithms: ["HS256"] })
-const jwtConfig = require('./../../helpers/auth.middleware');
 
-router.route('/').post(jwtConfig, otpCtrl.create)
-router.route('/').put(jwtConfig, otpCtrl.update)
-router.route('/').delete(jwtConfig, otpCtrl.destroy)
+router.route('/').post(requireAuth, otpCtrl.create)
+router.route('/').put(requireAuth, otpCtrl.update)
+router.route('/').delete(requireAuth, otpCtrl.destroy)
 
 module.exports = router
